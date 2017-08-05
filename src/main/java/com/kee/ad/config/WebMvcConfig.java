9727110e72c5d065089e4e39c7bfd8030b3249ef
KeeYang <yangqi@nongfadai.com>
2017-08-05 12:19:04 +0800

    add mvc config

---
+++ src/main/java/com/kee/ad/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.kee.ad.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author KeeYang on 2017/8/5.
+ * @Description :
+ */
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+}
