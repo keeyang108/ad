38c9d5eb0071ad20226a69b311c53d28af2ec1bf
qyang1 <qyang1@expedia.com>
2017-08-06 23:09:25 +0800

    add mvcConfig

--- src/main/java/com/kee/ad/config/WebMvcConfig.java
+++ src/main/java/com/kee/ad/config/WebMvcConfig.java
@@ -1,11 +1,46 @@
 package com.kee.ad.config;
 
+import com.kee.ad.interceptor.LoginInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author KeeYang on 2017/8/5.
  * @Description :
  */
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    private final static Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
+
+    protected boolean useSwagger;
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.debug("useSwagger:{}", useSwagger);
+        if (useSwagger) {
+            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
 }
--- src/main/java/com/kee/ad/interceptor/LoginInterceptor.java
+++ src/main/java/com/kee/ad/interceptor/LoginInterceptor.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -19,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  * 登录拦截器
  * Created by Administrator on 2016/11/15.
  */
+@Service
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Autowired
