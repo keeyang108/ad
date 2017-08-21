c8efecb3c0623ed7064eecee69cbb2bc774a74a2
KeeYang <yangqi@nongfadai.com>
2017-08-21 09:30:37 +0800

    add springbootServletInitialize to enable working in tomcat

--- src/main/java/com/kee/ad/AdApplication.java
+++ src/main/java/com/kee/ad/AdApplication.java
@@ -3,12 +3,19 @@ package com.kee.ad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication(scanBasePackages = {"com.kee.ad"})
 @ImportResource(locations = "classpath*:/spring/application-dao.xml")
-public class AdApplication {
+public class AdApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AdApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdApplication.class, args);
